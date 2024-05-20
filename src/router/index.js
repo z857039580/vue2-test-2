68e45a76c477d3d502973d10101120898f152a48
kevin_chu <9452714+kevin_chu@user.noreply.gitee.com>
2024-05-21 02:01:17 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AaTest from '@/views/AaTest.vue'
+import BbTest from '@/views/BbTest.vue'
 
 Vue.use(VueRouter)
 
@@ -17,8 +19,27 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/aa',
+    name: 'aa',
+    component: AaTest
+  },
+  {
+    path: '/bb',
+    name: 'bb',
+    component: BbTest
+  },
+  {
+    path: '/cc',
+    name: 'cc',
+    component: () => import(/* webpackChunkName: "about" */ '../views/CcTest.vue')
+  },
 ]
+
+export {routes};
+
+
 
 const router = new VueRouter({
   mode: 'history',
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,51 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+
+
+    <br>
+    <br>
+    <br>
+    <button @click="currentComponent = 'aa'">aa</button>
+    <button @click="currentComponent = 'bb'">bb</button>
+    <button @click="currentComponent = 'cc'">cc</button>
+    <br>
+    <br>
+    <br>
+
+    <template v-if="currentComponent">
+      <template v-for="item in links">
+        <component :key="item" v-show="currentComponent === item" :is="item"></component>
+      </template>
+    </template>
+
+
+
+
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
   </div>
 </template>
 
@@ -12,7 +56,23 @@ import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
+  data() {
+    return {
+      currentComponent: '',
+      links: ['aa', 'bb', 'cc']
+    };
+  },
+  mounted() {
+    import('../router').then(res => {
+      res.routes.forEach(item => {
+        // comp[item.name] = item.component;
+        this.$options.components[item.name] = item.component;
+      })
+
+      this.currentComponent = 'bb'
+    })
+  },
 }
 </script>
